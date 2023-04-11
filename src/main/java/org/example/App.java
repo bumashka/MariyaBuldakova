a0a0c2ef73a5102b7bd348962f0505e5d6272323
bumashka <90649137+bumashka@users.noreply.github.com>
2023-04-12 02:07:47 +0300

    Add existing project

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+import com.epam.tat.module4.Calculator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
